81eafc245a35e2a4f45199acade62f42273cea12
Lasy <1255352506@qq.com>
2019-11-22 17:56:48 +0800

    update: 简化功能和 API

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,38 +1,11 @@
-interface Page {
-    title: string;
-    keys: string[];
-    links: string[];
-    lastPinId: number;
-}
-declare const _default: {
-    new (id: number): {
-        id: number;
-        title: string;
-        keys: string[];
-        links: string[];
-        page: number;
-        amount: number;
-        /** 原始数据 */
-        originData: object;
-        status: number;
-        lastPinId: number;
-        /**
-         * 解析数据
-         * @param data 数据
-         */
-        parsePageData(data: any): Page;
-        /**
-         * 初始化（初始化的时候会获取第一页的数据并保存）
-         */
-        init(): Promise<any>;
-        /**
-         * 获取下一页图片信息
-         */
-        getNextPage(): Promise<Page | null | undefined>;
-        /**
-         * 直接获取所有图片信息
-         */
-        getAll(): Promise<void>;
-    };
-};
-export = _default;
+/**
+ * 根据 lastPinId 获取画板和一页的数据
+ * @param boardId 画板 id
+ * @param lastPinId 上页最后一张图片的 id
+ */
+export declare function getPageData(boardId: number, lastPinId?: number): Promise<object>;
+/**
+ * 转换图片的 key 为图片的链接
+ * @param keys 图片的 key
+ */
+export declare function convertKeysToLinks(keys: Array<string>): string[];
--- src/index.ts
+++ src/index.ts
@@ -15,178 +15,72 @@ interface Page {
   lastPinId: number
 }
 
+interface PinData {
+  pin_id: number
+  user_id: number
+  board_id: number
+  file_id: number
+  file: {
+    file_id: number
+    bucket: string
+    type: string
+    width: number
+    height: number
+    key: string
+    file_length: number
+    frames: number
+    [prop: string]: any
+  }
+}
+
+interface PageData {
+  board_id: number
+  user_id: number
+  title: string
+  description: string
+  category_id: string
+  seq: string
+  pin_count: number
+  follow_count: number
+  like_count: number
+  created_at: number
+  updated_at: number
+  deleting: number
+  is_private: number
+  extra: any
+  user: any
+  category_name: string
+  pins: PinData[]
+  [prop: string]: any
+}
+
 /**
  * 根据 lastPinId 获取画板和一页的数据
  * @param boardId 画板 id
  * @param lastPinId 上页最后一张图片的 id
  */
-function getData(boardId: number, lastPinId?: number): Promise<object> {
+export async function getPageData(
+  boardId: number,
+  lastPinId?: number
+): Promise<object> {
   const params = lastPinId ? { max: lastPinId } : {}
   const url = `${boardUrl}/${boardId}`
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  return axios({
+
+  const { data } = await axios({
     method: 'GET',
     url,
     headers: {
       'X-Requested-With': 'XMLHttpRequest'
     },
     params
-  }).then(res => {
-    const { data, headers } = res
-
-    // application/json
-    if (!/.*application\/json.*/.test(headers['content-type'])) {
-      throw new Error('Response "content-type" is not "application/json"')
-    }
-    return data.board
   })
+  return data.board as PageData
 }
 
 /**
  * 转换图片的 key 为图片的链接
  * @param keys 图片的 key
  */
-function convertKeysToLinks(keys: Array<string>) {
+export function convertKeysToLinks(keys: Array<string>) {
   return keys.map(key => `${imgBaseUrl}/${key}`)
-}
-
-export = class Board {
-  id: number
-  title: string
-  keys: string[]
-  links: string[]
-  page: number
-  amount: number
-  /** 原始数据 */
-  originData: object
-  protected status: number
-  private lastPinId: number
-  /**
-   * 构造函数
-   * @param id 画板 id
-   */
-
-  constructor(id: number) {
-    this.id = id
-    this.title = ''
-    this.keys = []
-    this.links = []
-    this.lastPinId = NO_PIN_ID
-    this.page = 0
-    this.amount = 0
-    this.status = NOT_INITED
-    this.originData = {}
-    Object.defineProperty(this, 'amount', {
-      get() {
-        return this.keys.length
-      },
-      set() {
-        console.warn('Can not set the value of "amount"')
-      }
-    })
-  }
-
-  /**
-   * 解析数据
-   * @param data 数据
-   */
-  private parsePageData(data: any): Page {
-    // 标题、pins
-    const { title, pins } = data
-
-    // keys、links
-    const keys: string[] = []
-    const pin_ids: number[] = []
-    pins.forEach((pin: any) => {
-      const { file, pin_id } = pin
-      keys.push(file.key)
-      pin_ids.push(pin_id)
-    })
-
-    // 完成
-    if (!keys.length) {
-      this.status = FINISHED
-      return {
-        title,
-        keys: [],
-        links: [],
-        lastPinId: NO_PIN_ID
-      }
-    }
-
-    // links
-    const links = convertKeysToLinks(keys)
-
-    // 该页最后一张图片的 pin_id
-    const lastPinId: number = pin_ids.length
-      ? pin_ids[pin_ids.length - 1]
-      : NO_PIN_ID
-
-    this.title = title
-    this.keys.push(...keys)
-    this.links.push(...links)
-    this.lastPinId = lastPinId
-
-    return { title, keys, lastPinId, links }
-  }
-
-  /**
-   * 初始化（初始化的时候会获取第一页的数据并保存）
-   */
-  async init(): Promise<Board> {
-    const data = await getData(this.id)
-    await this.parsePageData(data)
-    this.page = 1
-    this.status = INITED
-    this.originData = data
-    return this
-  }
-
-  /**
-   * 获取下一页图片信息
-   */
-  async getNextPage(): Promise<Page | undefined | null> {
-    if (!this.status) {
-      console.warn('Has not been inited')
-      return
-    }
-    const data = await getData(this.id, this.lastPinId)
-    const pageData = this.parsePageData(data)
-    if (this.status === FINISHED) {
-      return
-    }
-    this.status = PAGING
-    this.page++
-    return pageData
-  }
-
-  /**
-   * 直接获取所有图片信息
-   */
-  async getAll() {
-    // 初始化
-    !this.status && (await this.init())
-
-    // 下一次递归
-    const next = async () => {
-      // 完成
-      if (this.status === FINISHED) {
-        return
-      }
-
-      // 页数溢出
-      if (this.page >= MAX_PAGE) {
-        throw new Error(`Overflow: Max page: ${MAX_PAGE}`)
-      }
-
-      // 获取下一页数据
-      await this.getNextPage()
-
-      // 进入下一次递归
-      await next()
-    }
-
-    // 开始递归
-    await next()
-  }
 }
